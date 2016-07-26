dddbe16ae6c1a1e16e3cb36cbfc1d1d3c7a41309
yemelyn <pommy.shortcake@gmail.com>
2016-07-26 19:42:58 +0000

    Added AJAX add new contact

--- app/actions.rb
+++ app/actions.rb
@@ -1,4 +1,34 @@
 # Homepage (Root path)
 get '/' do
+  @contacts = Contact.all
+  @contact = Contact.new
   erb :index
+end
+
+post '/add_contact' do
+  @contact = Contact.create(
+    name: params[:name],
+    email: params[:email]
+    )
+  if request.xhr?
+    content_type :json
+    @contact.to_json
+  else
+    redirect '/'
+  end
+end
+
+get '/delete_contact' do
+  @delete_contact = Contact.find params[:id]
+  @delete_contact.destroy
+  if request.xhr?
+    content_type :json
+    @delete_contact.to_json
+  else
+    redirect '/'
+  end
+end
+
+get '/contact/edit' do
+  @contact = Contact.update
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,51 @@
-$(document).ready(function() {
+$(function () {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#new_contact').on('submit', function (event) {
+    event.preventDefault();
+
+    var form = $(this);
+
+    $.ajax({
+      method: form.attr('method'),
+      url: form.attr('action'),
+      data: form.serialize(),
+      success: function (data) {
+        // find the nut list
+        var contactList = $('.table').find($('tbody'));
+        // add a new list item to it
+        $('<tr>')
+          .append($('<td>')
+            .text(data.name)
+          )
+          .append($('<td>')
+            .append($('<a>').attr("href", "mailto:"+data.email)
+            .text(data.email))
+          )
+          .append($('<td>')
+            .append($('<button>').addClass('edit').text('Edit'))
+          )
+          .append($('<td>')
+            .append($('<button>').attr("name", "<%=id%>").addClass('delete').text('Delete'))
+          ).appendTo(contactList);
+      }
+    });
+
+  });
+
+
+
+  var button = $('#load-more-posts');
+  button.on('click', function () {
+    $.ajax({
+      url: 'more-posts.html',
+      method: 'GET',
+      success: function (morePostsHtml) {
+        button.replaceWith(morePostsHtml);
+      }
+    });
+  });
+
+
+
 });
+
